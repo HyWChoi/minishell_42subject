50a930bf79939c81c6922f9492e783a65287f364
HyWChoi <chw2309@icloud.com>
2024-05-01 19:38:07 +0900

    feat(builtins): implements a cd func

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -1,20 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tksh.h                                             :+:      :+:    :+:   */
+/*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyeonwch <hyeonwch@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/29 18:12:04 by yechakim          #+#    #+#             */
-/*   Updated: 2024/05/01 18:37:50 by hyeonwch         ###   ########.fr       */
+/*   Created: 2024/05/01 18:27:24 by hyeonwch          #+#    #+#             */
+/*   Updated: 2024/05/01 19:36:45 by hyeonwch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MS_H
-# define MS_H
+#include "../includes/tksh.h"
+#include <unistd.h>
+#include <stdio.h>
 
-#include "libft.h"
-
-void cd(char *path);
-
-#endif
+void	cd(char *path){
+	printf("target path: %s\n", path);
+	if (!chdir(path))
+	{
+		perror(" : No such file or directory");
+	}
+}
