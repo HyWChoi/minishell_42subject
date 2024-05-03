f4a95e61c6d415b4dec7b937dd7cbd56ccf30769
nerdchanii <nerdchanii@gmail.com>
2024-05-03 14:48:28 +0900

    feat(bultins): add exit functiion
    * feat(bultins): add exit functiion
    * feat(builtins): add exit fn signature in header

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -1,21 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tksh.h                                             :+:      :+:    :+:   */
+/*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yechakim <yechakim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/29 18:12:04 by yechakim          #+#    #+#             */
-/*   Updated: 2024/05/02 17:22:26 by yechakim         ###   ########.fr       */
+/*   Created: 2024/05/02 12:23:07 by yechakim          #+#    #+#             */
+/*   Updated: 2024/05/02 12:27:30 by yechakim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MS_H
-# define MS_H
+#include "tksh.h"
 
-#include "libft.h"
-
-void	exit_shell(unsigned char exit_code);
-void	unset(char **argv, char **envp);
-
-#endif
+/**
+ * @brief exit shell
+ * 
+ * This function is called when the user types the exit command.
+ * 
+ * @param exit_code exit code that will be ranged from 0 to 255
+ * @return void
+*/
+void	exit_shell(unsigned char exit_code)
+{
+	exit(exit_code);
+}
