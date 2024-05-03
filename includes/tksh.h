59b5e42541d7bf18311c833436ebd3cc86402b6a
HyWChoi <chw2309@icloud.com>
2024-05-03 18:37:08 +0900

    feat(builtins): add a struct for token

--- includes/tksh.h
+++ includes/tksh.h
@@ -1,21 +1,16 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   tksh.h                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hyeonwch <hyeonwch@student.42seoul.kr>     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/29 18:12:04 by yechakim          #+#    #+#             */
-/*   Updated: 2024/05/03 15:53:35 by hyeonwch         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef MS_H
 # define MS_H
 # define PWD_MAX_BUFF_SIZE 4096
 #include "libft.h"
 
-void cd(char *path);
+typedef struct	s_token
+{
+	char	*cmd_path;
+	char	**argv;
+	char	***envp;
+}	t_token;
+
+void	cd(char *path);
 void	pwd(void);
 void	env(const char **env);
 void	exit_shell(unsigned char exit_code);
