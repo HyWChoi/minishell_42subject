28b71ad30e9f2845e87db2153c161b33475525e6
HyWChoi <chw2309@icloud.com>
2024-05-10 21:26:05 +0900

    feat(builtins): implements a dto for env

---
+++ src/builtins/dtos/dto_env.c
@@ -0,0 +1,14 @@
+#include "tksh.h"
+#include "tksh_builtins.h"
+#include "libft.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void	dto_env(t_token *token)
+{
+	printf("-----token pass through the dto env-----\n");
+	if (*(*(token->envp)) == NULL)
+		return ;
+	if (ft_strncmp(token->cmd_path, "env", 3) == 0)
+		env(*(token->envp));
+}
