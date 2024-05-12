d47ffe789295fab00034c15fa1157f904ddd4bbf
HyWChoi <chw2309@icloud.com>
2024-05-12 17:37:49 +0900

    feat(builtins): implements a dto for unset

---
+++ src/builtins/dtos/dto_unset.c
@@ -0,0 +1,11 @@
+#include "tksh.h"
+#include "tksh_builtins.h"
+#include "libft.h"
+#include <stdio.h>
+
+void	dto_unset(t_token *token)
+{
+	printf("-----token pass through the dto cd-----\n");
+	if (ft_strncmp(token->cmd_path, "cd", 3) == 0)
+		unset(token->argv, token->envp);
+}
