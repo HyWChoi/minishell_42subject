ba04b5821c186ee051becbd3c1177f2a4bf868ec
nerdchanii <nerdchanii@gmail.com>
2024-05-17 13:37:20 +0900

    feat(builtin): implement dto of exit_shell

--- src/builtins/dtos/dto_exit.c
+++ src/builtins/dtos/dto_exit.c
@@ -5,7 +5,30 @@
 
 void	dto_exit(t_token *token)
 {
+	size_t argc;
+	char *exit_str;
+	
+	if (*token->argv == NULL)
+		exit_shell(0);
+	exit_str = token->argv[0];
+	argc = ft_strs_len((const char **)token->argv);
+	while (*exit_str)
+	{
+		if (!ft_isdigit(*exit_str))
+		{
+			printf("exit: %s: numeric argument required\n", *(token->argv));
+			exit_shell(255);
+		}
+		exit_str++;
+	}
+	if (2 <= argc) // TODO: argv의 구성에 따라 개수가 달라져야함
+	{
+		printf("exit: too many arguments\n");
+		// TODO: 시그널 처리 필요
+		return ;
+	}
 	printf("-----token pass through the dto exit-----\n");
 	if (ft_strncmp(token->cmd_path, "exit", 5) == 0)
-		exit(SIGKILL); // 임시로 넣은 SIG 추후 개선 필요
+		exit((unsigned char)ft_atoi(token->argv[0]));
 }
+
