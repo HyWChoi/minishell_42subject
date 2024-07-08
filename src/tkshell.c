49aff461bd8d7d74de9159da7ef07607ea3605e9
nerdchanii <nerdchanii@gmail.com>
2024-07-08 17:28:49 +0900

    setup tmpdir when execute sehll

--- src/tkshell.c
+++ src/tkshell.c
@@ -6,7 +6,7 @@
 /*   By: yechakim <yechakim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 14:37:09 by hyeonwch          #+#    #+#             */
-/*   Updated: 2024/07/05 15:31:31 by yechakim         ###   ########.fr       */
+/*   Updated: 2024/07/08 17:28:14 by yechakim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <readline/readline.h>
+#include <unistd.h>
+
+void setup_tty()
+{
+	int pid = fork();
+	int status;
+	const char *const tty = ttyname(STDIN_FILENO);
+	char *dirname;
+	
+	dirname = ft_strjoin(TK_HEREDOC_PATH, tty);
+	if (dirname == NULL)
+	{
+		ft_putendl_fd("cannot prepare the shell", 2);
+		exit(1);
+	}
+	if (pid == 0)
+	{
+		if (execve("/bin/mkdir", (char *[]){"mkdir", "-p", dirname, NULL}, NULL) == -1)
+		{
+			perror("cannot prepare the shell");
+			exit(1);
+		}	
+	}
+	free((void *)dirname);
+	waitpid(pid, &status, 0);
+	if (status != 0)
+	{
+		perror("cannot start the shell");
+		exit(1);
+	}
+}
+
 
 int main(int argc, char **argv, const char **initial_envp)
 {
@@ -32,14 +64,10 @@ int main(int argc, char **argv, const char **initial_envp)
 	char **tmp = ft_strs_copy(initial_envp);
 	envp = &tmp;
 	// set_exit_code(0);
+	setup_tty();
 	while (1)
 	{
 		char *origin_str = tksh_prompt(**envp);
-		// if(ft_strncmp(origin_str, "$?", 3) == 0)
-		// {
-		// 	printf("exit code: %d\n", exit_code);
-		// 	continue;
-		// }
 		token_list = prs_parse(origin_str, envp);
 		// dbg_print_token(token_list);
 		exit_code = execute(token_list);
