f40f10cea65963dc1241c5e1d5b802efbf4dfc94
nerdchanii <nerdchanii@gmail.com>
2024-07-09 11:34:52 +0900

    remove setuptty

--- src/tkshell.c
+++ src/tkshell.c
@@ -6,7 +6,7 @@
 /*   By: yechakim <yechakim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 14:37:09 by hyeonwch          #+#    #+#             */
-/*   Updated: 2024/07/08 17:28:14 by yechakim         ###   ########.fr       */
+/*   Updated: 2024/07/09 11:34:32 by yechakim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,37 +21,6 @@
 #include <readline/readline.h>
 #include <unistd.h>
 
-void setup_tty()
-{
-	int pid = fork();
-	int status;
-	const char *const tty = ttyname(STDIN_FILENO);
-	char *dirname;
-	
-	dirname = ft_strjoin(TK_HEREDOC_PATH, tty);
-	if (dirname == NULL)
-	{
-		ft_putendl_fd("cannot prepare the shell", 2);
-		exit(1);
-	}
-	if (pid == 0)
-	{
-		if (execve("/bin/mkdir", (char *[]){"mkdir", "-p", dirname, NULL}, NULL) == -1)
-		{
-			perror("cannot prepare the shell");
-			exit(1);
-		}	
-	}
-	free((void *)dirname);
-	waitpid(pid, &status, 0);
-	if (status != 0)
-	{
-		perror("cannot start the shell");
-		exit(1);
-	}
-}
-
-
 int main(int argc, char **argv, const char **initial_envp)
 {
 	t_token	**token_list;
@@ -64,7 +33,6 @@ int main(int argc, char **argv, const char **initial_envp)
 	char **tmp = ft_strs_copy(initial_envp);
 	envp = &tmp;
 	// set_exit_code(0);
-	setup_tty();
 	while (1)
 	{
 		char *origin_str = tksh_prompt(**envp);
