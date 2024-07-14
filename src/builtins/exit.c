cc862f6aae37bf773ea68fb7017fdaa52a21f363
HyWChoi <chw2309@icloud.com>
2024-07-14 15:16:10 +0900

    fix(builtins/exit): print exit when exit is executed

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: hyeonwch <hyeonwch@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 12:23:07 by yechakim          #+#    #+#             */
-/*   Updated: 2024/07/12 18:04:11 by hyeonwch         ###   ########.fr       */
+/*   Updated: 2024/07/14 15:14:52 by hyeonwch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 t_exit_code	exit_shell(unsigned char exit_code)
 {
+	printf("exit\n");
 	exit(exit_code);
 	return (exit_code);
 }
